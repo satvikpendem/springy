72023a1f52e0cc21e3322de71ce442212d700c38
Satvik Pendem <satvik.pendem@gmail.com>
2020-08-25 01:34:32 -0400

    Add AnimatedPositionedHookExample

--- lib/examples/animated_positioned_example.dart
+++ lib/examples/animated_positioned_example.dart
@@ -1,50 +1,93 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main(List<String> args) => runApp(const App());
 
-class App extends HookWidget {
+class Animator {
+  Animator({this.controller, this.animation});
+  AnimationController controller;
+  Animation<double> animation;
+}
+
+class App extends StatefulWidget {
   const App({Key key});
 
   static const Duration duration = Duration(milliseconds: 500);
   static const Curve curve = Curves.easeOutBack;
 
   @override
-  Widget build(BuildContext context) {
-    final AnimationController controller =
-        useAnimationController(duration: duration);
-    final Animation<double> animation = Tween<double>(
-      begin: 0,
-      end: 300,
-    )
-        .chain(
-          CurveTween(
-            curve: curve,
-          ),
-        )
-        .animate(controller);
-    final ValueNotifier<bool> isDown = useState<bool>(false);
-    final ValueNotifier<int> numBoxes = useState<int>(3);
+  _AppState createState() => _AppState();
+}
 
+class _AppState extends State<App> with TickerProviderStateMixin {
+  List<Animator> animators = [];
+  bool isDown = false;
+  int numBoxes = 3;
+
+  @override
+  void initState() {
+    for (int i = 0; i < numBoxes; i++) {
+      final AnimationController c = AnimationController(
+        duration: App.duration,
+        vsync: this,
+      );
+      animators.add(
+        Animator(
+          controller: c,
+          animation: Tween<double>(
+            begin: 0,
+            end: 300,
+          )
+              .chain(
+                CurveTween(
+                  curve: App.curve,
+                ),
+              )
+              .animate(c),
+        ),
+      );
+    }
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    for (int i = 0; i < numBoxes; i++) {
+      animators[i].controller.dispose();
+    }
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: SafeArea(
         child: Scaffold(
           floatingActionButton: FloatingActionButton(
             onPressed: () {
-              if (!isDown.value) {
-                controller.forward();
-                isDown.value = true;
+              if (!isDown) {
+                for (final Animator animator in animators) {
+                  animator.controller.forward();
+                }
+                setState(() {
+                  isDown = true;
+                });
               } else {
-                controller.reverse();
-                isDown.value = false;
+                for (final Animator animator in animators) {
+                  animator.controller.reverse();
+                }
+                setState(() {
+                  isDown = false;
+                });
               }
             },
           ),
-          body: AnimatedBuilder(
-            animation: animation,
-            builder: (_, __) => Boxes(
-              numBoxes: numBoxes.value,
-              animation: animation,
+          body: Stack(
+            children: List<Box>.generate(
+              numBoxes,
+              (int index) => Box(
+                index: index,
+                animation: animators[index].animation,
+              ),
             ),
           ),
         ),
@@ -53,31 +96,25 @@ class App extends HookWidget {
   }
 }
 
-class Boxes extends StatelessWidget {
-  Boxes({
-    @required this.numBoxes,
+class Box extends StatelessWidget {
+  Box({
     @required this.animation,
+    @required this.index,
   });
 
-  final int numBoxes;
+  final int index;
   final Animation<double> animation;
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: List<Widget>.generate(
-        numBoxes,
-        (int index) => Positioned(
-          top: (animation.value) + (index * (100 + 10)),
-          left: (MediaQuery.of(context).size.width - 100) / 2,
-          child: Container(
-            width: 100,
-            height: 100,
-            color: Colors.blue,
-          ),
-        ),
+    return Positioned(
+      top: (animation.value) + (index * (100 + 10)),
+      left: (MediaQuery.of(context).size.width - 100) / 2,
+      child: Container(
+        width: 100,
+        height: 100,
+        color: Colors.blue,
       ),
-      // ],
     );
   }
 }

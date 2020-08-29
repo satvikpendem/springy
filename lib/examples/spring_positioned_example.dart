fe1fa489c4e9bbf8b633c25f9464f6c94c504b39
Satvik Pendem <satvik.pendem@gmail.com>
2020-08-29 02:22:35 -0400

    Add foundation to SpringPositionedExample for debugFillProperties for functional_widget package

--- lib/examples/spring_positioned_example.dart
+++ lib/examples/spring_positioned_example.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:functional_widget_annotation/functional_widget_annotation.dart';

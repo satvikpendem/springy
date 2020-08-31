3f8e6e960cd5544562f91bb0f5667e3939be4963
Satvik Pendem <satvik.pendem@gmail.com>
2020-08-30 21:58:42 -0400

    Add docs for list extension methods

--- lib/util/list.dart
+++ lib/util/list.dart
@@ -1,4 +1,6 @@
+/// Methods to manuipulate elements in a list in a functional way
 extension ListManipulation<T> on List<T> {
+  /// Move [element] to end of list by removing and readding it
   List<T> moveToEnd(T element) => this
     ..remove(element)
     ..add(element);
